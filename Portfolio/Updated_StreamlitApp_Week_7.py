d50f726a5324ede7ebc7127936e0dd6bee6f4b06
samtchejeyan <sam.tchejeyan@tcu.edu>
2026-03-26 11:53:12 -0500

    Update Updated_StreamlitApp_Week_7.py

--- Portfolio/Updated_StreamlitApp_Week_7.py
+++ Portfolio/Updated_StreamlitApp_Week_7.py
@@ -58,7 +58,7 @@ df_features = extract_features_pair()
 MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
-        "pipeline": 'finalized_model.tar.gz',
+        "pipeline": 'finalized_pair_model.tar.gz',
         "keys": ["DLR", "AMZN"],
         "inputs": [{"name": k, "type": "number", "min": 0.0, "max": 1.0, "default": 0.0, "step": 10.0} for k in ["DLR", "AMZN"]]
 }
