36fb2005bb578afc1b1689222f87710bd9704325
samtchejeyan <sam.tchejeyan@tcu.edu>
2026-04-07 17:25:06 -0500

    Update StreamlitApp_HW5_Part_2.py

--- Portfolio/StreamlitApp_HW5_Part_2.py
+++ Portfolio/StreamlitApp_HW5_Part_2.py
@@ -118,7 +118,7 @@ def display_explanation(input_df, session, aws_bucket):
     
     preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[0:3]) 
     input_df_transformed = preprocessing_pipeline.transform(input_df) 
-    feature_names = best_pipeline[0:2].get_feature_names_out() 
+    feature_names = best_pipeline[0:3].get_feature_names_out() 
     input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names) 
     shap_values = explainer(input_df_transformed) 
 
