004620808fa10ce238c099a33911ea7166b07894
samtchejeyan <sam.tchejeyan@tcu.edu>
2026-04-14 16:32:13 -0500

    Update StreamlitApp_HW6.py

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -63,8 +63,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_sentiment.shap',
         "pipeline": 'finalized_sentiment_model.tar.gz',
-        "keys": ['ADBE','MSFT','JPM','sentiment_textblob'],
-        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['ADBE','MSFT','JPM','sentiment_textblob']]
+        "keys": ['ADBE','AMZN','WMT','PredictedSentiment'],
+        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['ADBE','AMZN','WMT','PredictedSentiment']]
 }
 
 def load_pipeline(_session, bucket, key):
